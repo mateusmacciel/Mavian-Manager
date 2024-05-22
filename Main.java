e90ffbb1f4d22a330822a0c26f0a4a45ee99588e
kelmerz <149977389+kelmerz@users.noreply.github.com>
2024-05-22 20:18:31 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,72 +1,14 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.border.Border;
 
-public class Main {
+
+class Main {
     public static void main (String[] args) {
-        JFrame frame = new JFrame("Gerador");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 800);
-
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(8,10,50,50));
-        panel.setBackground(new Color(0x00, 0x1d, 0x66));
-
-        JLabel label = new JLabel("Tamanho da senha:");
-        JTextField tamanhoField = new JTextField();
-
-        JButton gerarButton = new JButton("Gerar senha");
-        JLabel resultadolabel = new JLabel("Senha gerada:");
-        JTextField resultadoField = new JTextField();
-        resultadoField.setEditable(false);
-
-        Font font = new Font("Arial", Font.BOLD, 18);
-        Color textColor = Color.WHITE;
-
-        label.setFont(font);
-        label.setForeground(textColor);
-
-        tamanhoField.setFont(font);
-        tamanhoField.setForeground(textColor);
-        tamanhoField.setBackground(Color.DARK_GRAY);
-
-        gerarButton.setFont(font);
-        gerarButton.setForeground(textColor);
-        gerarButton.setBackground(new Color(0x00, 0x4d, 0xcc));
-        gerarButton.setPreferredSize(new Dimension(150,50));
-
-        resultadolabel.setFont(font);
-        resultadolabel.setForeground(textColor);
-
-        resultadoField.setFont(font);
-        resultadoField.setForeground(textColor);
-        resultadoField.setBackground(Color.DARK_GRAY);
-
-        panel.add(label);
-        panel.add(tamanhoField);
-        panel.add(gerarButton);
-        panel.add(new JLabel());
-        panel.add(resultadolabel);
-        panel.add(resultadoField);
-
-        frame.add(panel);
-
-        frame.setVisible(true);
-        
-        gerarButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                        int tamanho = Integer.parseInt(tamanhoField.getText());
-                        String senha = Gerador.gerarSenha(tamanho);
-                        resultadoField.setText(senha);
-                } catch (NumberFormatException nfe){
-                    JOptionPane.showMessageDialog(frame, "Por favor, insira um número.");
-                }
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Gerador();
+                //new ContaMestraInterface();
+                //new ContaServicoInterface();
             }
-        });
-    }
+        });
+    }
 }
-
