c150f19b617db1f669e9fdb8dc9954ecffa03b67
teusma <78906680+mateusmacciel@users.noreply.github.com>
2024-06-05 20:50:37 -0300

    Update Gerador.java

--- Gerador.java
+++ Gerador.java
@@ -2,58 +2,27 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.Random;
 
-class Cofre extends JFrame {
-    private JTextArea textArea;
-    private JButton consultarButton;
-    private JButton abrirCadastroButton;
-    private JButton abrirGeradorButton;
+class Gerador extends JFrame {
+    private JTextField tamanhoField;
+    private JPasswordField resultadoField;
+    
+    public static String gerarSenha(int tamanho) {
+        String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                "abcdefghijklmnopqrstuvwxyz" +
+                "0123456789" +
+                "!@#$%&*";
 
-    private class ConsultarListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            consultarContas();
+        StringBuilder sb = new StringBuilder();
+
+        Random random = new Random();
+
+        for (int i = 0; i < tamanho; i++) {
+            int indice = random.nextInt(caracteres.length());
+            sb.append(caracteres.charAt(indice));
         }
 
-        private void consultarContas() {
-            String url = "jdbc:postgresql://localhost:5432/meudb";
-            String dbUsuario = "postgres";
-            String dbSenha = "mateus";
-
-            try {
-                Connection connection = DriverManager.getConnection(url, dbUsuario, dbSenha);
-
-                String sql = "SELECT email, senha, site FROM mavian.contas";
-                PreparedStatement statement = connection.prepareStatement(sql);
-                ResultSet resultSet = statement.executeQuery();
-
-                StringBuilder stringBuilder = new StringBuilder();
-
-                while (resultSet.next()) {
-                    String email = resultSet.getString("email");
-                    String senhaHash = resultSet.getString("senha");
-                    String site = resultSet.getString("site");
-                    stringBuilder.append("Email: ").append(email).append("\n");
-                    stringBuilder.append("Senha Hash: ").append(senhaHash).append("\n");
-                    stringBuilder.append("Site: ").append(site).append("\n\n");
-                }
-
-                textArea.setText(stringBuilder.toString());
-
-                resultSet.close();
-                statement.close();
-                connection.close();
-            } catch (SQLException ex) {
-                JOptionPane.showMessageDialog(Cofre.this, "Erro ao consultar as contas no cofre: " + ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new Cofre());
+        return sb.toString();
     }
 }
